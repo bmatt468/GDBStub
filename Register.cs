32fc88c463c940173644d67f6fa2b08577e3888b
Daniel Recker <reckerdaniel@gmail.com>
2014-09-17 20:28:15 -0400

    Register holds 32 bit sized array and inherits from Memory, which is the Ram class from the loader phase

---
+++ Register.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GDBStub
+{
+    //Registers are a type of Memory.
+    class Register : Memory
+    {
+        //creates a register. they are all 32 bit or 4 bytes. yay!!!
+        public Register()
+        {
+            theArray = new byte[4];
+        }
+
+
+
+    }
+}
