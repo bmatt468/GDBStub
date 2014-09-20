733d573fd51c2710fba43793bb291580f6eda3c0
Benjamin R. Matthews <bnmtthews@gmail.com>
2014-09-20 16:05:14 -0400

    BRM 9/20/14 1604
    Additional work on Listen method in Handler. Began work with handshake.

--- Handler.cs
+++ Handler.cs
@@ -40,7 +40,7 @@ namespace GDBStub
                     TcpClient client = t.AcceptTcpClient();
                     Console.WriteLine("Connection established");
                     NetworkStream ns = client.GetStream();
-
+                    
                     int i = ns.Read(buffer, 0, buffer.Length);
 
                     while (i != 0)
@@ -50,6 +50,59 @@ namespace GDBStub
                         Console.WriteLine(String.Format("Received: {0}", data));
 
                         // Process the data sent by the client.
+                        switch (data)
+                        {                            
+                            case "+":
+                                //byte[] msg = System.Text.Encoding.ASCII.GetBytes("+");
+                                //ns.Write(msg, 0, msg.Length);
+                                break;
+
+                            case "-":
+                                //byte[] msg2 = System.Text.Encoding.ASCII.GetBytes("-");
+                                //ns.Write(msg2, 0, msg2.Length);
+                                break;
+
+                            default:
+                                string command;
+                                string checksum;
+                                ushort obtainedChecksum = 0;
+                                
+                                // get checksum
+                                checksum = data.Substring(data.IndexOf('#') + 1);
+                                data = data.Remove(data.IndexOf('#'));
+                                
+                                // get command
+                                command = data.Substring(data.IndexOf("$") + 1);
+                                
+
+                                // obtain checksum
+                                foreach (char c in command)
+                                {
+                                    obtainedChecksum += (ushort)Convert.ToInt16(c);                                   
+                                }
+                                obtainedChecksum %= 256;
+                                
+                                // send success packet if checksum matches
+                                if (checksum == obtainedChecksum.ToString("x"))
+                                {
+                                    byte[] msg3 = System.Text.Encoding.UTF8.GetBytes("+");
+                                    ns.Write(msg3, 0, msg3.Length);
+                                    Console.WriteLine(String.Format("Sent: {0}", System.Text.Encoding.UTF8.GetString(msg3)));
+                                }                                 
+                                // send failed packet if checksum doesn't match
+                                else
+                                {
+                                    byte[] msg3 = System.Text.Encoding.UTF8.GetBytes("-");
+                                    ns.Write(msg3, 0, msg3.Length);
+                                    Console.WriteLine(String.Format("Sent: {0}", System.Text.Encoding.UTF8.GetString(msg3)));
+                                    break;
+                                }
+
+                                // parse command
+                                this.ParseCommand(command, ns);
+                                
+                                break;
+                        }
                         
 
                         //data Processed
@@ -64,5 +117,47 @@ namespace GDBStub
                 Console.WriteLine(e.Message);
             }
         }
+
+        public void ParseCommand(string cmd, NetworkStream ns)
+        {
+            ushort chk = 0;
+            switch (cmd)
+            {
+                // initial phase of handshake
+                // server responds with packetsize (in hex)
+                // for testing purposes the respone is 119 (35 32-bit registers)
+                case "qSupported:qRelocInsn+":
+                    foreach (char c in "PacketSize=119")
+                    {
+                        chk += (ushort)Convert.ToInt16(c);
+                    }
+                    chk %= 256;
+                    byte[] msg = System.Text.Encoding.UTF8.GetBytes("$PacketSize=119#" + chk.ToString("x"));
+                    ns.Write(msg, 0, msg.Length);
+                    Console.WriteLine(String.Format("Sent: {0}", System.Text.Encoding.UTF8.GetString(msg)));
+                    break;
+
+                // phase of handshake
+                // server responds with packetsize (in hex)
+                // for testing purposes the respone is 119 (35 32-bit registers)
+                case "Hg0":
+                    string response = "OK";
+                    foreach (char c in response)
+                    {
+                        chk += (ushort)Convert.ToInt16(c);
+                    }
+                    chk %= 256;
+                    byte[] msg2 = System.Text.Encoding.UTF8.GetBytes("$" + response + "#" + chk.ToString("x"));
+                    ns.Write(msg2, 0, msg2.Length);
+                    Console.WriteLine(String.Format("Sent: {0}", System.Text.Encoding.UTF8.GetString(msg2)));
+                    break;
+                
+                default:
+                    byte[] msg335 = System.Text.Encoding.ASCII.GetBytes("");
+                    ns.Write(msg335, 0, msg335.Length);
+                    Console.WriteLine(String.Format("Sent: {0}", System.Text.Encoding.UTF8.GetString(msg335)));
+                    break;
+            }
+        }        
     }
 }
